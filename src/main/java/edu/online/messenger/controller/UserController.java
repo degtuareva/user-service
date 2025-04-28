fe6267079f579f784ec0c0133f66bf3e8c99cb16
olga.olegovna.603 <olga.olegovna.603@list.ru>
2025-04-28 14:25:28 +0300

    исправлены импорты в UserController

--- src/main/java/edu/online/messenger/controller/UserController.java
+++ src/main/java/edu/online/messenger/controller/UserController.java
@@ -4,7 +4,11 @@ import edu.online.messenger.model.dto.UserDto;
 import edu.online.messenger.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/users")
