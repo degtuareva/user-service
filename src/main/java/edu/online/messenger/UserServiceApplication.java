dfa1881628cf91a1b0e0f994c85170ae6eeb86a4
lvergunov <semenvatnik@gmail.com>
2025-05-26 22:31:42 +0300

    OMP-US-19. Создан миграция для таблицы Users.

--- src/main/java/edu/online/messenger/UserServiceApplication.java
+++ src/main/java/edu/online/messenger/UserServiceApplication.java
@@ -1,12 +1,24 @@
 package edu.online.messenger;
 
+import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class UserServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class);
+    }
+
+    @Bean
+    public SpringLiquibase liquibase(DataSource dataSource) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(dataSource);
+        liquibase.setChangeLog("classpath:changelog-master.xml");
+        return liquibase;
     }
 }
