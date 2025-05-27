827aba8a9fa4fe3aacf73bf09c23725a9b366162
lvergunov <semenvatnik@gmail.com>
2025-05-27 17:26:09 +0300

    OMP-US-19. Исправлены ошибки в миграции.

--- src/main/java/edu/online/messenger/configuration/LiquibaseConfiguration.java
+++ src/main/java/edu/online/messenger/configuration/LiquibaseConfiguration.java
@@ -1,21 +1,21 @@
-package edu.online.messenger.configuration;
-
-import liquibase.integration.spring.SpringLiquibase;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
-
-@Configuration
-public class LiquibaseConfiguration {
-
-    private static final String changelogString = "classpath:changelog-master.xml";
-    @Bean
-    public SpringLiquibase liquibase(DataSource dataSource) {
-        SpringLiquibase liquibase = new SpringLiquibase();
-        liquibase.setDropFirst(true);
-        liquibase.setDataSource(dataSource);
-        liquibase.setChangeLog(changelogString);
-        return liquibase;
-    }
-}
+//package edu.online.messenger.configuration;
+//
+//import liquibase.integration.spring.SpringLiquibase;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import javax.sql.DataSource;
+//
+//@Configuration
+//public class LiquibaseConfiguration {
+//
+//    private static final String changelogString = "classpath:changelog-master.xml";
+//    @Bean
+//    public SpringLiquibase liquibase(DataSource dataSource) {
+//        SpringLiquibase liquibase = new SpringLiquibase();
+//        liquibase.setDropFirst(true);
+//        liquibase.setDataSource(dataSource);
+//        liquibase.setChangeLog(changelogString);
+//        return liquibase;
+//    }
+//}
