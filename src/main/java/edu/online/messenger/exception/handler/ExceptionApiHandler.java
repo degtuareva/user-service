95654e5e6a6f03096121336e7405a9a55b26c805
lvergunov <semenvatnik@gmail.com>
2025-06-02 22:20:56 +0300

    OMP-US-23. Добавлена валидация и обработка ее ошибок.

--- src/main/java/edu/online/messenger/exception/handler/ExceptionApiHandler.java
+++ src/main/java/edu/online/messenger/exception/handler/ExceptionApiHandler.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -48,6 +49,13 @@ public class ExceptionApiHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ErrorDto> handleUserNotFoundException(IllegalArgumentException e) {
+        log.error(e.getLocalizedMessage());
+        return new ResponseEntity<>(new ErrorDto(e.getLocalizedMessage(), HttpStatus.BAD_REQUEST.value()),
+                HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorDto> handleUserNotFoundException(HttpMessageNotReadableException e) {
         log.error(e.getLocalizedMessage());
         return new ResponseEntity<>(new ErrorDto(e.getLocalizedMessage(), HttpStatus.BAD_REQUEST.value()),
                 HttpStatus.BAD_REQUEST);
