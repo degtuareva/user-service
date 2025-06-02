992a6c31d39051e3d465cde0648470ef0a99efb3
lvergunov <semenvatnik@gmail.com>
2025-06-02 21:52:09 +0300

    OMP-US-23. Добавлена валидация и обработка ее ошибок.

--- src/main/java/edu/online/messenger/exception/handler/ExceptionApiHandler.java
+++ src/main/java/edu/online/messenger/exception/handler/ExceptionApiHandler.java
@@ -6,10 +6,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class ExceptionApiHandler {
@@ -31,10 +35,15 @@ public class ExceptionApiHandler {
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorDto> handleUserNotFoundException(MethodArgumentNotValidException e) {
-        log.error(e.getLocalizedMessage());
-        return new ResponseEntity<>(new ErrorDto(e.getLocalizedMessage(), HttpStatus.BAD_REQUEST.value()),
-                HttpStatus.BAD_REQUEST);
+    public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            log.error("Validation error on field '{}': {}", fieldName, errorMessage);
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
