b779613fd98e4d9bfb10f3aec475639201971ea8
olga.olegovna.603 <olga.olegovna.603@list.ru>
2025-06-29 18:04:35 +0300

    feature/OMP-US-31:Откорректированы аннотации

--- src/test/java/edu/online/messenger/config/AbstractIntegrationTest.java
+++ src/test/java/edu/online/messenger/config/AbstractIntegrationTest.java
@@ -1,12 +1,14 @@
 package edu.online.messenger.config;
 
+import jakarta.transaction.Transactional;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Testcontainers
+@Transactional
+@ActiveProfiles("test")
 public abstract class AbstractIntegrationTest {
 
     @Container
